8df13c4da3141f52e29b39141da815f1f0b09cc7
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-01-12 15:45:38 +0000

    use GraphGraphviz to get GraphViz capabilities

--- src/ReverseDiffDebugUtils.jl
+++ src/ReverseDiffDebugUtils.jl
@@ -4,9 +4,11 @@ using ReverseDiff
 using Graphs, MetaGraphs
 using GraphPlot
 
+using GraphGraphviz
+
 using OrderedCollections: OrderedDict
 
-export make_graph, plothtml
+export make_graph, plothtml, plotgraphviz
 
 variable_label(x, index) = "x$(index)\n$(typeof(x))"
 variable_label(x::Real, index) = "x$(index)\n$(typeof(ReverseDiff.value(x)))"
@@ -148,6 +150,15 @@ function plothtml(g::MetaDiGraph; kwargs...)
         arrowlengthfrac=0.025,
         kwargs...
     )
+end
+
+plotgraphviz(f, args...; kwargs...) = plotgraphviz(make_graph(f, args...); kwargs...)
+function plotgraphviz(g::MetaDiGraph; kwargs...)
+    auto_kwargs = (
+        labels=["\"$(x)\"" for x in nodelabels(g)],
+        nodeedgecolors=nodecolors(g),
+    )
+    return to_graphviz(g; merge(auto_kwargs, kwargs)...)
 end
 
 end
